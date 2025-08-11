cf30aebb6e573aee29a184db31fef2fc8ed5c8f6
vijayasri9 <vijayasrimenda3@gmail.com>
2025-08-11 22:13:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+  
+function changeFontColor(color){
+    document.getElementById('text').style.color = color
+}
+
+function changeBgImage(link){
+    document.getElementById('text').style.backgroundImage = `url(${link})`
+}
+
+function changeBgColor(color){
+     document.getElementById('text').style.backgroundImage = null
+    document.getElementById('text').style.backgroundColor = color
+}
+
+function changeFontFamily(name){
+    document.getElementById('text').style.fontFamily = name
+}
+
+
+
+
+
